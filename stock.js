cf31a8ce6d2eac7c9dd9917d028a37a2fb140d63
kapsalonmediterraneo <robertcroes1993@hotmail.com>
2025-09-07 23:32:10 +0200

    stock.js

--- stock.js
+++ stock.js
@@ -1,6 +1,6 @@
 // stock.js
 // Edita aquí las cantidades disponibles
 const stock = {
-  fohn: 2,   // Föhn disponibles
+  fohn: 10,   // Föhn disponibles
   callos: 10  // Máquinas quita callos disponibles
 };
