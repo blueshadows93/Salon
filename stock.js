aaa6288663e1f31cffbe66693eaa0e157be4b06f
kapsalonmediterraneo <robertcroes1993@hotmail.com>
2025-09-07 23:17:41 +0200

    stock.js

--- stock.js
+++ stock.js
@@ -1,5 +1,4 @@
-// Aquí defines el stock disponible
 const stock = {
-  fohn: 1,     // cantidad de secadores disponibles
-  callos: 8    // cantidad de máquinas quita callos disponibles
+  fohn: 2,   // ahora quedan 2 secadores
+  callos: 0  // agotado
 };
